11a3a63be37197e578b50a08c83685a86614b351
Haruto Ino <a18247h@reitaku.jp>
2021-05-15 16:11:27 +0900

    fix-error-getting-property-6

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -110,8 +110,8 @@ class ItemController extends Controller
     {
         // $item = Item::where('id', $item)->with('user')->first();       
         $item = Item::find($item);
-        
-        if(Auth::id() != $item['user_id']){
+        $itemUserId = optional($item)->user_id;
+        if(Auth::id() !=  $itemUserId){
             return abort('403');
         }
 
@@ -131,7 +131,8 @@ class ItemController extends Controller
         // 画像アップデート：画像を消す→新規追加
         // $item = Item::where('id', $item)->with('user')->first();
         $item = Item::find($item);
-        if(Auth::id() != $item['user_id']) {
+        $itemUserId = optional($item)->user_id;
+        if(Auth::id() != $itemUserId) {
             return abort('403');
         }
 
