ed81c15a447a6933e4314e8a191dd850bf96b0fc
Haruto Ino <a18247h@reitaku.jp>
2021-05-15 15:54:12 +0900

    fix-error-getting-property-2

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -111,7 +111,8 @@ class ItemController extends Controller
         // $item = Item::where('id', $item)->with('user')->first();       
         $item = Item::find($item);
 
-        if(Auth::id() != $item->user_id){
+        $itemUserId = optional($item)->user_id;
+        if(Auth::id() !=  $itemUserId){
             return abort('403');
         }
 
